7eacbdb6e7e2c000c4c849e81498ae7abff5ebe4
Sachin Sharma <ssharma0355@gmail.com>
2024-01-27 02:06:20 +0530

    first commit

---
+++ vite-project/src/components/Hero.jsx
@@ -0,0 +1,32 @@
+const Hero=()=>{
+
+
+    return (
+        <main className="hero container">
+            <div className="hero-content">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                 
+                 <div className="hero-btn"> 
+                 <button> Shop Now</button>
+                 <button className="seconday-btn"> Category</button>
+                 </div>
+                 <div className="shoping">
+                    <p>Also avilable on </p>
+                    <div className="brand-icon">
+                        <img src="../src/assets/images/amazon.png" alt="Amazon" />
+                        <img src="../src/assets/images/flipkart.png" alt="Flipkart" />    
+
+                    </div>
+                 </div>
+                 </div>
+
+                 <div className="hero-image">
+                        <img src="../src/assets/images/shoe_image.png" alt="Amazon" />
+
+                 </div>
+
+        </main>
+    )
+}
+export default Hero;
